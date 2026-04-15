cc39e6c1bec7b99eb06bf690cdb9b37c1748d983
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-04-15 18:49:23 +0200

    add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import Header from "./ui/header";
 
 function App() {
-
   return (
     <>
-      
+      <Header />
+      <main></main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
