8b8d3ece3f05b05e5c0263e47f0a60ee122a2857
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-04-15 19:26:11 +0200

    small ideas

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,5 +1,13 @@
 import "./header.css";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <a href="#">Start</a>
+      <a href="#">ResultsGame</a>
+      <a href="#">Explanations</a>
+      <a href="#">Graphs</a>
+      <a href="#">Map</a>
+    </header>
+  );
 }
