3bad3004f62196ad44dadc74befd71a3b0e88b99
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-04-17 10:58:00 +0200

    formating

--- src/ui/game/index.tsx
+++ src/ui/game/index.tsx
@@ -1,10 +1,10 @@
 import Section from "../section";
 import Game from "./three-setup";
 
-export default function GameSection({id}:{id:string}) {
-    return (
-        <Section id={id} width="full">
-                  <Game />
-                </Section>
-    );
+export default function GameSection({ id }: { id: string }) {
+  return (
+    <Section id={id} width="full">
+      <Game />
+    </Section>
+  );
 }
